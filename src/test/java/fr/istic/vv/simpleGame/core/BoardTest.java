ebaa241b2b24e7796f369fa9e58a01b3e8f8835f
StephaneMangin <stephane.mangin@freesbee.fr>
2015-09-30 15:06:31 +0200

    Test structure added

---
+++ src/test/java/fr/istic/vv/simpleGame/core/BoardTest.java
@@ -0,0 +1,81 @@
+package fr.istic.vv.simpleGame.core;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by stephane on 30/09/15.
+ */
+public class BoardTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGetXSize() throws Exception {
+
+    }
+
+    @Test
+    public void testGetYSize() throws Exception {
+
+    }
+
+    @Test
+    public void testGetSquareContent() throws Exception {
+
+    }
+
+    @Test
+    public void testRemovePawn() throws Exception {
+
+    }
+
+    @Test
+    public void testAddPawn() throws Exception {
+
+    }
+
+    @Test
+    public void testIsBonusSquare() throws Exception {
+
+    }
+
+    @Test
+    public void testNumberOfPawns() throws Exception {
+
+    }
+
+    @Test
+    public void testMaxGold() throws Exception {
+
+    }
+
+    @Test
+    public void testGetNextPawn() throws Exception {
+
+    }
+
+    @Test
+    public void testSquareContentSprite() throws Exception {
+
+    }
+
+    @Test
+    public void testToString() throws Exception {
+
+    }
+
+    @Test
+    public void testRemoveAllPawns() throws Exception {
+
+    }
+}
---
+++ src/test/java/fr/istic/vv/simpleGame/core/PawnTest.java
@@ -0,0 +1,53 @@
+package fr.istic.vv.simpleGame.core;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by stephane on 30/09/15.
+ */
+public class PawnTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGetX() throws Exception {
+
+    }
+
+    @Test
+    public void testGetY() throws Exception {
+
+    }
+
+    @Test
+    public void testGetLetter() throws Exception {
+
+    }
+
+    @Test
+    public void testGetGold() throws Exception {
+
+    }
+
+    @Test
+    public void testMove() throws Exception {
+
+    }
+
+    @Test
+    public void testIsDead() throws Exception {
+
+    }
+}
